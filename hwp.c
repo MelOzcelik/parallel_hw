1b27473e3c79ea3a1eff2ae083b3bcb1b40ff0d8
MelOzcelik <136354439+MelOzcelik@users.noreply.github.com>
2023-10-28 23:12:41 -0400

    Add files via upload

---
+++ hwp.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+#define NUMSTEPS 1000000
+
+int main(int argc, char** argv) {
+    int i, rank, size;
+    double x, pi, sum = 0.0;
+    double step = 1.0 / (double) NUMSTEPS;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int local_steps = NUMSTEPS / size;
+    int local_start = rank * local_steps;
+    int local_end = local_start + local_steps;
+    x = (local_start + 0.5) * step;
+
+    for (i = local_start; i < local_end; i++) {
+        x += step;
+        sum += 4.0 / (1.0 + x * x);
+    }
+
+    MPI_Reduce(&sum, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        pi *= step;
+        printf("PI is %.20f\n", pi);
+    }
+
+    MPI_Finalize();
+
+    return 0;
